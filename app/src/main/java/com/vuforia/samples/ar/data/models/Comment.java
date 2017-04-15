5b26c86b77df549a1b14f9ebd2035ab9a6b8df08
grishberg <grishberg@MacBook-Pro-Grigory.local>
2017-04-15 19:24:09 +0700

    добавил аннотацию Data

--- app/src/main/java/com/vuforia/samples/ar/data/models/Comment.java
+++ app/src/main/java/com/vuforia/samples/ar/data/models/Comment.java
@@ -1,9 +1,11 @@
 package com.vuforia.samples.ar.data.models;
 
+import lombok.Data;
+
 /**
  * Created by grishberg on 15.04.17.
  */
-
+@Data
 public class Comment {
     private String text;
     private UserInfo owner;
