f8c9632df9d4925c5696781008eed108e5269f76
VoronovVS <dev@vvoronov.ru>
2017-04-15 20:58:22 +0700

    Added products details screen

---
+++ app/src/main/java/com/vuforia/samples/VuforiaSamples/app/ImageTargets/ImageTargetTouchListener.java
@@ -0,0 +1,15 @@
+package com.vuforia.samples.VuforiaSamples.app.ImageTargets;
+
+import android.support.annotation.Nullable;
+
+import com.vuforia.samples.ar.data.models.ProductInfo;
+
+interface ImageTargetTouchListener {
+
+    @Nullable
+    ProductInfo getCurrentProductInfo();
+
+    void onDetailsClicked(ProductInfo productInfo);
+
+    void onAutofocusRequested();
+}
