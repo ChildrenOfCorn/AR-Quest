62765cdd9fb32f90ac07bd795878a21fe9a60171
grishberg <grishberg@MacBook-Pro-Grigory.local>
2017-04-16 10:31:50 +0700

    сделал модели сериализуемыми

--- app/src/main/java/com/vuforia/samples/ar/data/beans/Comment.java
+++ app/src/main/java/com/vuforia/samples/ar/data/beans/Comment.java
@@ -1,12 +1,14 @@
 package com.vuforia.samples.ar.data.beans;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 /**
  * Created by grishberg on 15.04.17.
  */
 @Data
-public class Comment {
+public class Comment implements Serializable{
     public Comment(final String text, final UserInfo owner) {
         this.text = text;
         this.owner = owner;
