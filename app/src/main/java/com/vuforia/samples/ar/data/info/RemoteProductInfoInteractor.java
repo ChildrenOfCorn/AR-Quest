4a664b10c8abd1e6a8b46bc84caeb178f359e333
grishberg <grishberg@MacBook-Pro-Grigory.local>
2017-04-16 09:06:56 +0700

    добавил InfoRepository

---
+++ app/src/main/java/com/vuforia/samples/ar/data/info/RemoteProductInfoInteractor.java
@@ -0,0 +1,21 @@
+package com.vuforia.samples.ar.data.info;
+
+import javax.inject.Inject;
+
+/**
+ * Created by grishberg on 16.04.17.
+ */
+
+public class RemoteProductInfoInteractor implements ProductInfoInteractor {
+
+    @Inject Prod
+    @Override
+    public void getProductInfoByTargetId(long recognizedTargetId) {
+
+    }
+
+    @Override
+    public void setOnProductReceivedListener(OnProductReceivedListener listener) {
+
+    }
+}
